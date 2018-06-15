4fc722fa6da66c125bd6c7e72d2ac292e8725b70
Bogdan Fusa <bogdan.fusa@endava.com>
2018-06-16 01:02:14 +0300

    addedhost option

---
+++ voting-app/src/main/java/com/vote/app/controller/HostController.java
@@ -0,0 +1,25 @@
+package com.vote.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/host")
+public class HostController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String getComputerName()
+    {
+        Map<String, String> env = System.getenv();
+        if (env.containsKey("COMPUTERNAME"))
+            return env.get("COMPUTERNAME");
+        else if (env.containsKey("HOSTNAME"))
+            return env.get("HOSTNAME");
+        else
+            return "Unknown Computer";
+    }
+
+}
